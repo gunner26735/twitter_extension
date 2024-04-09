a092d1c0f6d071628fa37fd4b161208e2935e345
gunner26735 <sidshukla2016@gmail.com>
2024-04-09 18:42:33 +0530

    added UI changes

--- content.js
+++ content.js
@@ -4,21 +4,23 @@ function something(){
     const elements = document.querySelectorAll('div[class="DraftEditor-editorContainer"]');
     elements.forEach((input)=>{
         const button = document.createElement('button');
-        button.textContent = 'Click Me';
+        button.classList.add("tweetcp");
+        button.textContent = 'TCP';
+        button.setAttribute('style',"background-color: #603FEF;border:none;border-radius: 10px;color: white;font-size: 0.5em");
         input.parentNode.insertBefore(button, input.nextSibling);
         
         button.addEventListener('click', () => {
             console.log('Button clicked!');
 
             const tweetText = document.querySelectorAll('div[data-testid="tweetText"]');
-            tweetText.forEach((e)=>{
-                console.log(e.innerHTML);
+            const toPasteTweet = document.querySelectorAll('div[class="public-DraftStyleDefault-block public-DraftStyleDefault-ltr"]');
+            toPasteTweet.forEach((paste)=>{
+                paste.innerHTML = tweetText[0].innerHTML;
             })
         });
         flag = false;
         flag===false?clearInterval(caller):null;
     })
-    
 }
 if(flag){
     caller = setInterval(something,2000);
